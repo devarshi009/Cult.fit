07c17343d6c132e026c56f9aa33f2c5e7150d9c7
devarshi009 <devarshiojha009@gmail.com>
2022-12-18 11:55:48 +0530

    day-4

--- cult-fit/src/Pages/Home.jsx
+++ cult-fit/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState,useEffect} from "react";
 import {
   Button,
   Box,
@@ -19,7 +19,28 @@ import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar";
 
 const Home = () => {
-  return (
+  const [loading__Status, setLoading__Status] = useState(true);
+
+  useEffect(() => {
+    let setIntervalId = setInterval(() => {
+      setLoading__Status(false);
+    }, 1000);
+
+    return () => {
+      clearInterval(setIntervalId);
+    };
+  }, []);
+
+  return loading__Status ? (
+    <div className="loading__Screen">
+      {" "}
+      <img style={{margin:"400px 700px"}}
+        src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_1.0700000524520874,f_auto,fl_progressive//image/loader_compressed-v3.gif"
+        alt="loading-gif"
+      />
+    </div>
+  ):(
+  
     <div className="homePage">
       {/* <Navbar/> */}
       {/* HOMEPAGE FIRST VEDIO POSTER STARTS */}
